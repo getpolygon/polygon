4ec15ae856623f0e2993bc6d4f1f391214c301d8
Michael <michael.grigoryan25@gmail.com>
2020-12-13 14:53:41 +0400

    Updated UI Components

--- public/components/video-component.mjs
+++ public/components/video-component.mjs
@@ -3,9 +3,9 @@ import Footer from "/components/partials/Footer.mjs";
 class VideoComponent {
   create(postId, authorImage, authorId, author, text, video, postDate, { readOnly }) {
     function checkReadOnly() {
-      if (readOnly) return;
-      else
+      if (!readOnly)
         return `<i style="float: right" postId="${postId}" class="submitDeleteForm fas fa-trash-alt p-2" role="button"></i>`;
+      else return "";
     }
     return `
       <div id="${postId}" class="post container shadow-sm rounded-lg mt-1 mb-4 pr-4 pl-4 pb-3 pt-3 bg-white">

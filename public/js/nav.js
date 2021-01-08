de4f2bfa4afd11dd90aea2fbcaaa6c4f13f84952
Michael <michael.grigoryan25@gmail.com>
2021-01-08 21:57:18 +0400

    Minor UI update

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,26 +1,28 @@
-let logoutForm = document.getElementById("logout");
-let logoutButton = document.getElementById("submitLogout");
-let searchBox = document.getElementById("search");
+const logoutForm = document.getElementById("logout");
+const logoutButton = document.getElementById("submitLogout");
+const searchBox = document.getElementById("search");
+const fullName = document.querySelector(".nav-account-name");
+
+// This is to add 3 dots if the full name has 10+ characters
+const trimmed_full_name = (str, speclength) => {
+  if (str.length > speclength) return `${str.slice(0, speclength)}...`;
+  else return str;
+};
+
+fullName.innerHTML = trimmed_full_name(fullName.innerHTML, 10);
 
 function search() {
   fetch(`/api/search/?query=${searchBox.value}`)
     .then((doc) => doc.json())
     .then((doc) => {
-      let foundItemsContainer = document.getElementById("foundItems");
-
+      const foundItemsContainer = document.getElementById("foundItems");
       if (searchBox.value == "") {
         foundItemsContainer.innerHTML = "";
       } else {
         doc.forEach((result) => {
-          let dupl = document.getElementById(result._id);
-
+          const dupl = document.getElementById(result._id);
           if (!dupl) {
-            let el = document.createElement("a");
-            // This is to add 3 dots if the full name has 10+ characters
-            var trimmed_full_name = (str, speclength) => {
-              if (str.length > speclength) return `${str.slice(0, speclength)}...`;
-              else return str;
-            };
+            const el = document.createElement("a");
             el.classList.add("p-3");
             el.classList.add("mt-1");
             el.classList.add("shadow-sm");

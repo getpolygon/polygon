b89c517aaa2e300eaa0a9a2518975e64d1e41bbd
Michael Grigoryan <michael.grigoryan25@gmail.com>
2021-01-26 18:48:55 +0400

    Improved Registration back-end

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -40,67 +40,73 @@ router.post("/", upload.single("avatar"), async (req, res) => {
   const hasValidEmail = emailValidator.validate(email);
   const hasDuplicates = await _checkForDuplicates({ email: email }, AccountSchema);
 
-  if (hasValidEmail && !hasDuplicates) {
-    bcrypt.genSalt(10, (err, salt) => {
-      if (err) console.error(err);
-      else if (salt) {
-        bcrypt.hash(req.body.password, salt, async (err, hash) => {
-          if (err) console.error(err);
-          else if (hash) {
-            const Account = new AccountSchema({
-              _id: new mongoose.Types.ObjectId(),
-              firstName: req.body.firstName,
-              lastName: req.body.lastName,
-              fullName: `${req.body.firstName} ${req.body.lastName}`,
-              email: email,
-              bio: req.body.bio,
-              password: hash,
-              isPrivate: req.body.privateCheck ? true : false,
-              friends: {
-                pending: [],
-                approved: [],
-                requested: []
-              },
-              date: Date.now()
-            });
+  if (hasValidEmail) {
+    if (!hasDuplicates) {
+      bcrypt.genSalt(10, (err, salt) => {
+        if (err) console.error(err);
+        else if (salt) {
+          bcrypt.hash(req.body.password, salt, async (err, hash) => {
+            if (err) console.error(err);
+            else if (hash) {
+              const Account = new AccountSchema({
+                _id: new mongoose.Types.ObjectId(),
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                fullName: `${req.body.firstName} ${req.body.lastName}`,
+                email: email,
+                bio: req.body.bio,
+                password: hash,
+                isPrivate: req.body.privateCheck ? true : false,
+                friends: {
+                  pending: [],
+                  approved: [],
+                  requested: []
+                },
+                date: Date.now()
+              });
 
-            if (req.file) {
-              MinIOClient.fPutObject(
-                MINIO_BUCKET,
-                `${Account._id}/${Account._id}.png`,
-                req.file.path,
-                {
-                  "Content-Type": req.file.mimetype
-                }
-              );
-              const presignedUrl = await MinIOClient.presignedGetObject(
-                MINIO_BUCKET,
-                `${Account._id}/${Account._id}.png`
-              );
-              Account.pictureUrl = presignedUrl;
-              unlinkSync(path.resolve("tmp", req.file.originalname));
-            } else {
-              Account.pictureUrl = `https://avatars.dicebear.com/api/initials/${Account.fullName}.svg`;
-            }
-
-            await Account.save();
-            jwt.sign({ id: Account._id }, process.env.JWT_TOKEN, (err, token) => {
-              if (err) console.log(err);
-              else if (token) {
-                return res
-                  .status(201)
-                  .cookie("jwt", token, {
-                    httpOnly: true
-                  })
-                  .json({
-                    message: "Account Created"
-                  });
+              if (req.file) {
+                MinIOClient.fPutObject(
+                  MINIO_BUCKET,
+                  `${Account._id}/${Account._id}.png`,
+                  req.file.path,
+                  {
+                    "Content-Type": req.file.mimetype
+                  }
+                );
+                const presignedUrl = await MinIOClient.presignedGetObject(
+                  MINIO_BUCKET,
+                  `${Account._id}/${Account._id}.png`
+                );
+                Account.pictureUrl = presignedUrl;
+                unlinkSync(path.resolve("tmp", req.file.originalname));
+              } else {
+                Account.pictureUrl = `https://avatars.dicebear.com/api/initials/${Account.fullName}.svg`;
               }
-            });
-          }
-        });
-      }
-    });
+
+              await Account.save();
+              jwt.sign({ id: Account._id }, process.env.JWT_TOKEN, (err, token) => {
+                if (err) console.log(err);
+                else if (token) {
+                  return res
+                    .status(201)
+                    .cookie("jwt", token, {
+                      httpOnly: true
+                    })
+                    .json({
+                      message: "Account Created"
+                    });
+                }
+              });
+            }
+          });
+        }
+      });
+    } else {
+      return res.json({
+        error: "Forbidden"
+      });
+    }
   } else {
     return res.json({
       error: "Invalid email"

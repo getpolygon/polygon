4ac404062e2d1967b4bab451bc07f96facad5dc1
Michael Grigoryan <michael.grigoryan25@gmail.com>
2021-04-02 22:41:16 +0400

    Deleted containers

--- containers/minio/start.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-minio server /data
--- src/utils/omit.js
+++ src/utils/omit.js
@@ -4,4 +4,5 @@ const safeStringify = require("fast-safe-stringify").default;
 const omit = (obj, keys) => {
 	return _.omit(JSON.parse(safeStringify(obj)), keys);
 };
+
 module.exports = omit;

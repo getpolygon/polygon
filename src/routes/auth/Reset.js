73f6f55fdaf16611bbfdaefda5be63a815dce126
Michael Grigoryan <michael.grigoryan25@gmail.com>
2021-06-25 19:45:18 +0400

    Deleted reset password functionality for now

--- src/routes/auth/Reset.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const ResetController = require("../../controllers/auth/Reset.controller");
-
-router.post("/", ResetController.resetPassword);
-// router.post("/validate", ResetController.validate);
-
-module.exports = router;

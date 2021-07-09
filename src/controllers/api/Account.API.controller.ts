4c1ad4b97cc26621b1f226aa4ce1eb562e18d32d
Michael Grigoryan <56165400+MichaelGrigoryan25@users.noreply.github.com>
2021-07-09 16:45:05 +0400

    Updated Account fetching API

--- src/controllers/api/Account.API.controller.ts
+++ src/controllers/api/Account.API.controller.ts
@@ -16,31 +16,71 @@ export const fetchAccount = async (
     // Get current user
     const {
       rows: { 0: user },
-    } = await slonik.query<User>(sql`
-      SELECT * FROM users WHERE id = ${req.user?.id!!}
-    `);
+    } = await slonik.query(sql`
+      SELECT 
+        Account.*, 
+        to_json(
+          array_remove(
+            array_agg(Posts), NULL
+          )
+        ) AS posts,
+        to_json(
+          array_remove(
+            array_agg(Comments), NULL
+          )
+        ) AS comments
+      FROM users Account
 
-    // Get all posts
-    const { rows: posts } = await slonik.query(sql`
-      SELECT * FROM posts WHERE user_id = ${req.user?.id!!}
-    `);
+      LEFT OUTER JOIN (SELECT * FROM posts) Posts ON Posts.user_id = Account.id
+      LEFT OUTER JOIN (
+        SELECT 
+          Comment.*, 
+          to_json(Post) as post
+          
+        FROM comments Comment
 
-    // Get all comments
-    const { rows: comments } = await slonik.query(sql`
-      SELECT * FROM comments WHERE user_id = ${req.user?.id!!}
-    `);
+        LEFT OUTER JOIN posts Post ON Comment.post_id = Post.id
+      ) Comments ON Comments.user_id = Account.id
 
-    user.posts = posts as [];
-    user.comments = comments as [];
+      WHERE Account.username = ${req.user?.username!!}
+      GROUP BY Account.id, Posts.*, Comments.*;
+    `);
 
     return res.json(user);
   } else {
     // Find the user
     const {
       rows: { 0: user },
-    } = await slonik.query<User>(sql`
-      SELECT * FROM users WHERE username = ${username.toString()} LIMIT 1
+    } = await slonik.query(sql`
+      SELECT 
+        Account.*, 
+        to_json(
+          array_remove(
+            array_agg(Posts), NULL
+          )
+        ) AS posts,
+        to_json(
+          array_remove(
+            array_agg(Comments), NULL
+          )
+        ) AS comments
+      FROM users Account
+
+      LEFT OUTER JOIN (SELECT * FROM posts) Posts ON Posts.user_id = Account.id
+      LEFT OUTER JOIN (
+        SELECT 
+          Comment.*, 
+          to_json(Post) as post
+          
+        FROM comments Comment
+
+        LEFT OUTER JOIN posts Post ON Comment.post_id = Post.id
+      ) Comments ON Comments.user_id = Account.id
+
+      WHERE Account.username = ${req.user?.username!!}
+      GROUP BY Account.id, Posts.*, Comments.*;
     `);
+
     // If user does not exist
     if (!user) return res.status(404).send();
     else return res.json(user);

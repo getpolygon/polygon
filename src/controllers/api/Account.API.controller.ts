9c7207f8eea9f196fb8c4f70a571a8c4a1bd9563
Michael Grigoryan <56165400+MichaelGrigoryan25@users.noreply.github.com>
2021-07-09 16:55:19 +0400

    Optimized some code for Account API

--- src/controllers/api/Account.API.controller.ts
+++ src/controllers/api/Account.API.controller.ts
@@ -1,7 +1,6 @@
 import { sql } from "slonik";
 import Express from "express";
 import slonik from "../../db/slonik";
-import { User } from "../../@types";
 
 // For fetching account details
 export const fetchAccount = async (
@@ -11,12 +10,9 @@ export const fetchAccount = async (
   // Get the username from the query
   const { username } = req.query;
 
-  // If no account ID was provided
-  if (!username) {
-    // Get current user
-    const {
-      rows: { 0: user },
-    } = await slonik.query(sql`
+  const {
+    rows: { 0: user },
+  } = await slonik.query(sql`
       SELECT 
         Account.*, 
         to_json(
@@ -42,49 +38,14 @@ export const fetchAccount = async (
         LEFT OUTER JOIN posts Post ON Comment.post_id = Post.id
       ) Comments ON Comments.user_id = Account.id
 
-      WHERE Account.username = ${req.user?.username!!}
+      WHERE Account.username = ${
+        !username ? req.user?.username!! : username.toString()
+      }
       GROUP BY Account.id, Posts.*, Comments.*;
     `);
 
-    return res.json(user);
-  } else {
-    // Find the user
-    const {
-      rows: { 0: user },
-    } = await slonik.query(sql`
-      SELECT 
-        Account.*, 
-        to_json(
-          array_remove(
-            array_agg(Posts), NULL
-          )
-        ) AS posts,
-        to_json(
-          array_remove(
-            array_agg(Comments), NULL
-          )
-        ) AS comments
-      FROM users Account
-
-      LEFT OUTER JOIN (SELECT * FROM posts) Posts ON Posts.user_id = Account.id
-      LEFT OUTER JOIN (
-        SELECT 
-          Comment.*, 
-          to_json(Post) as post
-          
-        FROM comments Comment
-
-        LEFT OUTER JOIN posts Post ON Comment.post_id = Post.id
-      ) Comments ON Comments.user_id = Account.id
-
-      WHERE Account.username = ${req.user?.username!!}
-      GROUP BY Account.id, Posts.*, Comments.*;
-    `);
-
-    // If user does not exist
-    if (!user) return res.status(404).send();
-    else return res.json(user);
-  }
+  if (!user) return res.status(404).send();
+  else return res.json(user);
 };
 
 // For deleting account

e2cddc8c809097050df65ec46b0d4ce8f2d396a1
Michael Grigoryan <michael.grigoryan25@gmail.com>
2021-07-17 17:24:57 +0400

    Fixed a logout bug

--- src/controllers/auth/Logout.controller.ts
+++ src/controllers/auth/Logout.controller.ts
@@ -3,6 +3,15 @@ import Express from "express";
 export default (req: Express.Request, res: Express.Response) => {
   const { jwt: token } = req.signedCookies;
 
-  if (token) return res.status(200).clearCookie("jwt").json();
+  if (token)
+    return res
+      .status(200)
+      .clearCookie("jwt", {
+        signed: true,
+        secure: true,
+        httpOnly: true,
+        sameSite: "none",
+      })
+      .json();
   else return res.status(403).json();
 };

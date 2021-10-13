5c99372471d0d5deb424518fc32780d593ef62ae
michaelgrigoryan25 <michael.grigoryan25@gmail.com>
2021-10-13 23:14:08 +0400

    Switched to ts-node with nodemon for development

---
+++ src/types/express/index.d.ts
@@ -0,0 +1,14 @@
+import type { User } from "../user";
+
+declare module "express" {
+  interface Request {
+    user?: Partial<User>;
+    signedCookies: {
+      jwt?: string;
+    };
+  }
+
+  interface Response {
+    user?: Partial<User>;
+  }
+}
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,24 +1,4 @@
-import type { User } from "./user";
-
 // Custom declarations
 export type Token = {
   id: string;
 };
-
-/**
- * Global declarations
- */
-declare global {
-  namespace Express {
-    export interface Request {
-      user?: Partial<User>;
-      signedCookies?: {
-        jwt?: string;
-      };
-    }
-
-    export interface Response {
-      user?: Partial<User>;
-    }
-  }
-}

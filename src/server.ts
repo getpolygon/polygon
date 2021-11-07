6dfb5df1a667556d3d96eef067a7e0a32ea74cd0
Michael Grigoryan <michael.grigoryan25@gmail.com>
2021-11-07 18:40:13 +0400

    Code refactor

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+import app from "./app";
+import http from "http";
+import morgan from "morgan";
+import logger from "./util/Logger";
+import errorHandler from "errorhandler";
+
+// Port for Polygon to bind to
+const port = process.env.PORT || process.env.POLYGON_PORT || 3001;
+
+// Applying development middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+
+  // For printing the full stacktrace
+  app.use(errorHandler());
+}
+
+http
+  .createServer(app)
+  .listen(port, () => logger.info(`Backend started at port ${port}`));

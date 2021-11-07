858e1a194124049b78acf63c8a032da482a5c7de
Michael Grigoryan <michael.grigoryan25@gmail.com>
2021-11-07 18:40:13 +0400

    Code refactor

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,21 @@
-require("dotenv").config();
-
-import http from "http";
 import cors from "cors";
-import morgan from "morgan";
 import helmet from "helmet";
 import routes from "./routes";
 import express from "express";
-import logger from "./util/Logger";
 import compression from "compression";
 import cookieParser from "cookie-parser";
 
 const app = express();
-const port = process.env.PORT || 3001;
-const isDev = process.env.NODE_ENV === "development";
-const origins = isDev
-  ? true
-  : JSON.parse((process.env.ORIGINS as any) || null) || false;
+const origins = JSON.parse(process.env.POLYGON_ORIGINS!!) || true;
 
 // Middleware
 app.use(helmet());
 app.use(compression());
 app.use(express.json());
-isDev && app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(cors({ origin: origins, credentials: true }));
 
 app.use(routes);
 
-const httpServer = http.createServer(app);
-httpServer.listen(port, () => logger.info(`Backend started at port ${port}`));
+export default app;

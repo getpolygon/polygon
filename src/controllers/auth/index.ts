9d3e7c2e1a25eb6c89f80baed988434fb782e1a9
Michael Grigoryan <56165400+michaelgrigoryan25@users.noreply.github.com>
2021-11-08 17:40:41 +0400

    Removed logout endpoint.
    Removing the `logout` endpoint, since the token will be stored on client-side and will be revoked once invalid

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -1,6 +1,5 @@
 import login from "./login";
-import logout from "./logout";
 import verify from "./verify";
 import register from "./register";
 
-export { login, logout, verify, register };
+export { login, verify, register };
--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,12 +1,10 @@
 import login from "./login";
 import express from "express";
 import register from "./register";
-import { logout } from "../../controllers/auth";
 import forbidAuthenticated from "../../middleware/forbidAuthenticated";
 
 const router = express.Router();
 
-router.post("/logout", logout);
 router.use("/login", forbidAuthenticated(), login);
 router.use("/register", forbidAuthenticated(), register);
 

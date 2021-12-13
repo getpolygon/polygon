dd035e025cf20a085095873d99cda957d28c4293
Michael Grigoryan <56165400+michaelgrigoryan25@users.noreply.github.com>
2021-12-13 18:32:17 +0400

    Moved tests to project root

--- spec/itOrDefaultTo.spec.ts
+++ spec/itOrDefaultTo.spec.ts
@@ -1,6 +1,6 @@
 import test from "ava";
 import { itOrDefaultTo } from "lib/itOrDefaultTo";
 
-test("`itOrDefaultTo()` doesn't fail", (t) => {
+test("`itOrDefaultTo()` should not fail", (t) => {
   t.is(itOrDefaultTo(null, "default"), "default");
 });

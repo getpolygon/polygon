4ee35cbb8e616076e15756fe40760df4601aba7d
Michael Grigoryan <56165400+michaelgrigoryan25@users.noreply.github.com>
2022-01-03 00:06:17 +0400

    Replaced raw SQL call with a DAO call

--- src/controllers/api/users/close.ts
+++ src/controllers/api/users/close.ts
@@ -1,10 +1,10 @@
-import pg from "db/pg";
+import { userDao } from "container";
 import type { Request, Response } from "express";
 
 // For closing an account
 const close = async (req: Request, res: Response) => {
   try {
-    await pg.query("DELETE FROM users WHERE id = $1;", [req.user?.id]);
+    await userDao.deleteUserById(req.user?.id!);
     return res.sendStatus(204);
   } catch (error) {
     console.error(error);

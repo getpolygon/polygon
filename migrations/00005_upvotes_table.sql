0f8214a379d1ee551a464266fa9df05bff9f3265
Michael Grigoryan <56165400+michaelgrigoryan25@users.noreply.github.com>
2022-01-04 15:56:52 +0400

    Updated `upvotes` table migrations

--- migrations/00005_upvotes_table.sql
+++ migrations/00005_upvotes_table.sql
@@ -1,7 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE upvotes (
-  id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
   user_id UUID NOT NULL REFERENCES users(id),
   post_id UUID NOT NULL REFERENCES posts(id),
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
--- migrations/00025_upvotes_user_id_key.sql
+++ migrations/00025_upvotes_user_id_key.sql
@@ -1,10 +1,10 @@
 -- +goose NO TRANSACTION
 -- +goose Up
 -- +goose StatementBegin
-CREATE UNIQUE INDEX CONCURRENTLY upvotes_id_pkey ON upvotes(id);
+CREATE UNIQUE INDEX CONCURRENTLY upvotes_user_id_key ON upvotes(user_id);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP INDEX CONCURRENTLY upvotes_id_pkey;
+DROP INDEX CONCURRENTLY upvotes_user_id_key;
 -- +goose StatementEnd

0695f96a4159a722fe5aae9a4815043dca8ab13e
Michael Grigoryan <56165400+michaelgrigoryan25@users.noreply.github.com>
2022-01-15 19:52:35 +0400

    Minor patch

--- src/app/index.ts
+++ src/app/index.ts
@@ -22,7 +22,7 @@ app.use(
   cors({
     credentials: true,
     methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
-    origin: [config.polygon.frontend].concat(...config.polygon.origins),
+    origin: [config.polygon.frontend].concat(config.polygon.origins),
   })
 );
 

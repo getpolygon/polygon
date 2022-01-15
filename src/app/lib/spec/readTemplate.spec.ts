8fd2c0de9536b8209aacf0225616b7b31f5614d2
Michael Grigoryan <56165400+michaelgrigoryan25@users.noreply.github.com>
2022-01-15 19:25:07 +0400

    Delete readTemplate.spec.ts

--- src/app/lib/spec/readTemplate.spec.ts
+++
@@ -1,8 +0,0 @@
-import test from "ava";
-// required for reading `config` used by `readTemplate`
-import "reflect-metadata";
-import { readTemplate } from "../readTemplate";
-
-test("`readTemplate()` should throw an error", async (t) => {
-  await t.throwsAsync(async () => await readTemplate("nonexistent"));
-});
